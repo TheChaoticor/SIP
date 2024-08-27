72b8e3c633ef2624a646979384df031c472c5f2f
Arpit Mishra <arpitbbsrboy@gmail.com>
2024-08-27 22:52:35 +0530

    added chatbot

--- Indo_Utsav/src/App.jsx
+++ Indo_Utsav/src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import Navigation from './Components/Navigation'
-import Hero from './Components/Hero'
-import './App.css'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import viteLogo from '/vite.svg';
+import Navigation from './Components/Navigation';
+import Hero from './Components/Hero';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.id = 'messenger-widget-b';
+    script.src = 'https://cdn.botpenguin.com/website-bot.js';
+    script.defer = true;
+    script.text = '66cd8b596f3d13030b924fe2,6610ae11eef8b629a228ae36';
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
 
   return (
     <>
-      <Navigation/>
-      <Hero/>
+      <Navigation />
+      <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
